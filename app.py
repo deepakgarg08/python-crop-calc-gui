cd2188ac2e945774de181851135387c18d329a5b
Deepak <erdkgarg08@gmail.com>
2020-10-06 23:33:20 +0200

    working fine with comments added

--- app.py
+++ app.py
@@ -23,57 +23,35 @@ from flask_cors import CORS
 
 
 # In[2]:
-
-
-df = pd.read_csv('database.csv')
+# xls, df1_Z141, df2_Z351, df3_Z231, df4_Z121, df5_Z251, df6_Z31, df = None, None, None, None, None, None, None, None
+# xls = pd.ExcelFile('Corrected.xlsx')
+# df1_Z141 = pd.read_excel(xls, 'Sheet9')
+# df2_Z351 = pd.read_excel(xls, 'Sheet17')
+# df3_Z231 = pd.read_excel(xls, 'Sheet12')
+# df4_Z121 = pd.read_excel(xls, 'Sheet7')
+# df5_Z251 = pd.read_excel(xls, 'Sheet14')
+# df6_Z31 = pd.read_excel(xls, 'Sheet3')
+# df = pd.read_excel(xls)
 
 
 # In[3]:
 
 
-new_header = df.iloc[0] 
-df = df[1:]
-df.columns = new_header
+# new_header = df.iloc[0] 
+# df = df[1:]
+# df.columns = new_header
 res = {}
-
 
 # In[4]:
 
 
-df.head()
+# df.head()
 
 
 # In[ ]:
 
 
-def calculateCalibrationValidation(f2_cal,f3_cal,f1_val,f2_val,f3_val):
-    #f2_yield calibration
-    if (f2_cal):
-        f2_yield = 1545.24 + float(df.loc[f2_cal]['Z141']) * 0.3971 + float(df.loc[f2_cal]['Z351']) * 0.1693 + f2_cal * 50.53
-        res["f2_yield_calib"] = round(f2_yield, 2)
-       
-    #f3_yield ... calibration
-    if (f3_cal):
-        f3_yield = 1406.01+ float(df.loc[f3_cal]['Z231']) * 0.685 + float(df.loc[f3_cal]['Z141']) * 0.353 + f3_cal * 50.11 
-        res["f3_yield_calib"] =  round(f3_yield, 2)
-    
-    #fi_yield...validation
-    if (f1_val):
-        f1_yield = 2566.3 + float(df.loc[f1_val]['Z121']) * 2.525 + float(df.loc[f1_val]['Z31']) * 21.8 + f1_val * 47.43 
-        res["f1_yield_valid"] = round(f1_yield, 2)
-    
-    #fi_yield...validation
-    if (f2_val):
-        f2_yield = 2327.93 + float(df.loc[f2_val]['Z231']) * 1.15 + float(df.loc[f2_val]['Z251']) * 0.45 + f2_val * 47.60
-        res["f2_yield_valid"] =   round(f2_yield, 2)  
-    
-    #fi_yield...validation
-    if (f3_val):
-        f3_yield = 2163.26 + float(df.loc[f3_val]['Z141']) * 0.25 + float(df.loc[f3_val]['Z351']) * 0.16+ f3_val * 48.11
-        res["f3_yield_valid"] =     round(f3_yield, 2)
-    
-    print("result is :" , res)
-    return res
+
 
 app = Flask(__name__)
 CORS(app)
@@ -84,6 +62,51 @@ def index():
 	# return "ohh yeah!!"
 	return render_template("index.html")
 
+@app.route("/upload", methods = ['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        global xls, df1_Z141, df2_Z351, df3_Z231, df4_Z121, df5_Z251, df6_Z31
+        xls = df1_Z141 = df2_Z351 = df3_Z231 = df4_Z121 = df5_Z251 = df6_Z31 = None
+        f = request.files['file']
+        xls = pd.ExcelFile(f)
+        df1_Z141 = pd.read_excel(xls, 'Sheet9')
+        df2_Z351 = pd.read_excel(xls, 'Sheet17')
+        df3_Z231 = pd.read_excel(xls, 'Sheet12')
+        df4_Z121 = pd.read_excel(xls, 'Sheet7')
+        df5_Z251 = pd.read_excel(xls, 'Sheet14')
+        df6_Z31 = pd.read_excel(xls, 'Sheet3')
+    return render_template("index.html")   
+
+def calculateCalibrationValidation(f2_cal,f3_cal,f1_val,f2_val,f3_val):
+    #f2_yield calibration
+    if (f2_cal):
+        f2_yield = 1545.24 + float(df1_Z141.loc[f2_cal]['Z141']) * 0.3971 + float(df2_Z351.loc[f2_cal]['Z351']) * 0.1693 + f2_cal * 50.53
+        res["f2_yield_calib"] = round(f2_yield, 2)
+        print("z141")
+        print(df1_Z141.loc[f2_cal]['Z141'])
+       
+    #f3_yield ... calibration
+    if (f3_cal):
+        f3_yield = 1406.01+ float(df3_Z231.loc[f3_cal]['Z231']) * 0.685 + float(df1_Z141.loc[f3_cal]['Z141']) * 0.353 + f3_cal * 50.11 
+        res["f3_yield_calib"] =  round(f3_yield, 2)
+    
+    #fi_yield...validation
+    if (f1_val):
+        f1_yield = 2566.3 + float(df4_Z121.loc[f1_val]['Z121']) * 2.525 + float(df6_Z31.loc[f1_val]['Z31']) * 21.8 + f1_val * 47.43 
+        res["f1_yield_valid"] = round(f1_yield, 2)
+    
+    #fi_yield...validation
+    if (f2_val):
+        f2_yield = 2327.93 + float(df3_Z231.loc[f2_val]['Z231']) * 1.15 + float(df5_Z251.loc[f2_val]['Z251']) * 0.45 + f2_val * 47.60
+        res["f2_yield_valid"] =   round(f2_yield, 2)  
+    
+    #fi_yield...validation
+    if (f3_val):
+        f3_yield = 2163.26 + float(df1_Z141.loc[f3_val]['Z141']) * 0.25 + float(df2_Z351.loc[f3_val]['Z351']) * 0.16+ f3_val * 48.11
+        res["f3_yield_valid"] =     round(f3_yield, 2)
+    
+    print("result is :" , res)
+    return res
 
 @app.route('/calc',methods=['POST'])
 def createEmp():   
@@ -96,11 +119,9 @@ def createEmp():
     return result
 
 
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(debug=True,host='0.0.0.0', port=3000)
 
 
 # In[ ]:
@@ -109,21 +130,6 @@ if __name__ == '__main__':
 # [f2_cal,f3_cal,f1_val,f2_val,f3_val] = int(input()),int(input()),int(input()),int(input()),int(input())
 # result = calculateCalibrationValidation(f2_cal,f3_cal,f1_val,f2_val,f3_val)
 # print(result)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
